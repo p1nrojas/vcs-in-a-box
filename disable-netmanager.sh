ee85e5a19c46a86cb62653a8e905a8d0a8e0d1ad
Mauricio Rojas <mroja001@usmovnmroja001.attlocal.net>
2017-01-31 15:46:08 -0600

    Creation of VMs working for NS, VSD and VSC. NS is being configured nicely. Now It's time to clean up

--- disable-netmanager.sh
+++
@@ -1,7 +0,0 @@
-guestfish -a /tmp/vsd40r61.qcow2 -i ln-sf /dev/null /etc/systemd/system/NetworkManager.service
-guestfish -a /tmp/vsd40r61.qcow2 -i ln-sf /dev/null /etc/systemd/system/NetworkManager-dispatcher.service
-guestfish -a /tmp/vsd40r61.qcow2 -i ln-sf /dev/null /etc/systemd/system/NetworkManager-wait-online.service
-
-# Removed symlink /etc/systemd/system/multi-user.target.wants/NetworkManager.service.
-# Removed symlink /etc/systemd/system/dbus-org.freedesktop.NetworkManager.service.
-# Removed symlink /etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service.
