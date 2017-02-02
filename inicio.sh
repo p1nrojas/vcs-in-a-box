e95f2e2a3d7d0389562292c2e4fad890e86154be
Mauricio Rojas <mroja001@usmovnmroja001.attlocal.net>
2017-02-02 15:53:34 -0600

    cleaning feb 2, 2016

--- inicio.sh
+++ inicio.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-command -v ansible-playbook >/dev/null 2>&1 || { echo >&2 "I require "ansible-playbook" but it's not installed.  Aborting."; exit 1; }
+# ssh-keyscan -t rsa 192.168.1.113 | sed -e 's/#.*$//' | sed -e '/^$/d' >> ~/.ssh/known_hosts  # Adding hypervisor to known_hosts
 
 # Uncomment just in case you need to create bridges and dummy interfaces
 # ansible-playbook _bridges.yml 
 
 ## Here we go!
-echo "$(date -R) Here we go!"
+echo "$(date) Here we go!"
 # Start creating your servers
-echo "$(date -R) Start creating your servers"
+echo "$(date) Start creating your servers"
 ansible-playbook -i hosts nuage-install.yml
 
 # Configure your DNS, NTP and DHCP server
 sleep 150 # Wait for server to come online
 ssh-keyscan -t rsa 10.10.10.2 | sed -e 's/#.*$//' | sed -e '/^$/d' >> ~/.ssh/known_hosts  # Adding nserver to known_hosts
-echo "$(date -R) Configuring nserver"
+echo "$(date) Configuring nserver"
 ansible-playbook -i hosts nserver-deploy.yml
 
 # Finish installation in  your VSD  server
-ssh-keyscan -t rsa 10.10.10.10 | sed -e 's/#.*$//' | sed -e '/^$/d' >> ~/.ssh/known_hosts  # Adding nserver to known_hosts
-echo "$(date -R) finishing installation of VSD"
+ssh-keyscan -t rsa 10.10.10.10 | sed -e 's/#.*$//' | sed -e '/^$/d' >> ~/.ssh/known_hosts  # Adding vsd to known_hosts
+echo "$(date) finishing installation of VSD"
 ansible-playbook -i hosts nuage-deploy.yml 
 
-echo "$(date -R) Listo! ahora juegue!" 
+echo "$(date) Listo! ahora juegue!" 
